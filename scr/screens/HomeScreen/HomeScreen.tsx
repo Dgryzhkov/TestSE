d80ffc173abccd7ff6c3da3f473360dc57b78252
«Denis» <ryjkov_denis@smartworld.team>
2024-08-08 13:33:44 +0700

    add check internet

--- scr/screens/HomeScreen/HomeScreen.tsx
+++ scr/screens/HomeScreen/HomeScreen.tsx
@@ -1,22 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {
   View,
-  FlatList,
-  Image,
   Text,
-  RefreshControl,
-  TouchableOpacity,
+  FlatList,
   ActivityIndicator,
+  TouchableOpacity,
+  Image,
+  RefreshControl,
   ImageStyle,
 } from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
 import {observer} from 'mobx-react-lite';
-import photoStore from '../../app/store/store';
-import {useNavigation} from '@react-navigation/native';
 
-import {StackNavigationProp} from '@react-navigation/stack';
 import getStyle from './HomeScreenStyles';
+import photoStore from '../../app/store/store';
 import CustomModal from '../../widget/CustomModal';
-import Domains from '../../shared/config/Domains';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {useNavigation} from '@react-navigation/native';
 
 type RootStackParamList = {
   Home: undefined;
@@ -25,22 +25,34 @@ type RootStackParamList = {
 
 type NavigationProps = StackNavigationProp<RootStackParamList, 'Home'>;
 
-const HomeScreen = observer(() => {
+const HomeScreen = observer(({}) => {
+  const [isConnected, setIsConnected] = useState(true);
   const navigation = useNavigation<NavigationProps>();
-  const styles = getStyle();
-  useEffect(() => {
-    photoStore.fetchPhotos();
-  }, []);
 
-  const handleLoadMore = () => {
-    if (!photoStore.loading) {
+  const styles = getStyle();
+
+  const handleRefresh = useCallback(() => {
+    photoStore.reset();
+    if (isConnected) {
       photoStore.fetchPhotos();
     }
-  };
+  }, [isConnected]);
 
-  const handleRefresh = () => {
-    photoStore.reset();
-    photoStore.fetchPhotos();
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected ?? false);
+      if (state.isConnected) {
+        handleRefresh();
+      }
+    });
+
+    return () => unsubscribe();
+  }, [handleRefresh, isConnected]);
+
+  const handleLoadMore = () => {
+    if (!photoStore.loading && isConnected) {
+      photoStore.fetchPhotos();
+    }
   };
 
   const handlePhotoPress = (photoUrl: string) => {
@@ -57,11 +69,18 @@ const HomeScreen = observer(() => {
         />
       )}
 
-      {!photoStore.loading && photoStore.photos.length === 0 && (
+      {!photoStore.loading && !isConnected && (
+        <Text style={styles.noPhotosText}>
+          Нет подключения к интернету, если при подключении данные не
+          подгрузились перезайдите пожалйста в приложение
+        </Text>
+      )}
+
+      {!photoStore.loading && isConnected && photoStore.photos.length === 0 && (
         <Text style={styles.noPhotosText}>Нет доступных фотографий</Text>
       )}
 
-      {!photoStore.loading && photoStore.photos.length > 0 && (
+      {!photoStore.loading && isConnected && photoStore.photos.length > 0 && (
         <FlatList
           data={photoStore.photos}
           keyExtractor={item => item.id.toString()}
